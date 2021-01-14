a283cdad7ab62dc365550af996a5c89947575115
qasidd <SGurung@academytrainee.com>
2021-01-14 16:24:57 +0000

    orders_items domain class

---
+++ src/main/java/com/qa/ims/persistence/domain/OrdersItems.java
@@ -0,0 +1,86 @@
+package com.qa.ims.persistence.domain;
+
+public class OrdersItems {
+	
+	private Long id;
+	private Long orderId;
+	private Long itemId;
+	
+	public OrdersItems(Long id, Long orderId, Long itemId) {
+		this.id = id;
+		this.orderId = orderId;
+		this.itemId = itemId;
+	}
+	
+	public OrdersItems(Long orderId, Long itemId) {
+		this.orderId = orderId;
+		this.itemId = itemId;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdersItems other = (OrdersItems) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemId == null) {
+			if (other.itemId != null)
+				return false;
+		} else if (!itemId.equals(other.itemId))
+			return false;
+		if (orderId == null) {
+			if (other.orderId != null)
+				return false;
+		} else if (!orderId.equals(other.orderId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "OrdersItems [id=" + id + ", orderId=" + orderId + ", itemId=" + itemId + "]";
+	}
+
+}
