3ca653d6ad6ab41fe96d109aa7db9d3e6ae38eb2
qasidd <SGurung@academytrainee.com>
2021-01-19 16:00:57 +0000

    added getInt, getDouble and getLong to utils

--- src/main/java/com/qa/ims/utils/Utils.java
+++ src/main/java/com/qa/ims/utils/Utils.java
@@ -1,8 +1,15 @@
 package com.qa.ims.utils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDaoMysql;
+
 public class Utils {
+	
+	public static final Logger LOGGER = Logger.getLogger(Utils.class);
 	
 	private Utils () {
 		
@@ -13,5 +20,43 @@ public class Utils {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.nextLine();
 	}
-
+	
+	public static int getInt() {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			try {
+				return scanner.nextInt();
+			} catch (InputMismatchException e) {
+				LOGGER.error(e.getMessage());
+				LOGGER.info("Please enter only a number!");
+			}
+		}
+	}
+	
+	public static double getDouble() {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			try {
+				return scanner.nextDouble();
+			} catch (InputMismatchException e) {
+				LOGGER.error(e.getMessage());
+				LOGGER.info("Please enter only a decimal!");
+			}
+		}
+	}
+	
+	public static long getLong() {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			try {
+				return scanner.nextLong();
+			} catch (InputMismatchException e) {
+				LOGGER.error(e.getMessage());
+				LOGGER.info("Please enter only a number!");
+			}
+		}
+	}
 }
